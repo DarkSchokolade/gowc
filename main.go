f459c5d21e5fe9fd1eafaf19f515aa375b82ea1c
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2023-10-03 23:39:42 +0530

    stage 2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
@@ -9,21 +10,28 @@ import (
 
 func main() {
 	count_bytes := flag.Bool("c", false, "Byte count")
+	count_lines := flag.Bool("l", false, "Line count")
 	flag.Parse()
 	files := flag.Args()
 
 	fmt.Println(files)
 
 	var byte_count int64
+	var line_count int
 	for _, file := range files {
 		if *count_bytes {
 			byte_count = BytesInAFile(file)
+			fmt.Printf("%d\t", byte_count)
 		}
-		fmt.Printf("%d %s", byte_count, file)
+		if *count_lines {
+			line_count = LinesInAFile(file)
+			fmt.Printf("%d\t", line_count)
+		}
+		fmt.Println(file)
 	}
 }
 
-// Returns number of bytes
+// Returns number of bytes in a file
 func BytesInAFile(filename string) int64 {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -31,4 +39,26 @@ func BytesInAFile(filename string) int64 {
 	}
 	number_of_bytes := len(data)
 	return int64(number_of_bytes)
+}
+
+// Returns number of lines in a file
+func LinesInAFile(filepath string) int {
+	total_lines := 0
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		total_lines++
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return total_lines
 }
