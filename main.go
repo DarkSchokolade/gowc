0de7ff177c43f97fe8689cd7d3eed857030cd623
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2023-10-07 12:38:11 +0530

    Stage 5 changed everything

--- main.go
+++ main.go
@@ -4,43 +4,156 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/DarkSchokolade/gowc/wc"
 )
 
-func main() {
-	count_lines := flag.Bool("l", false, "Print Line count")
-	count_words := flag.Bool("w", false, "Print Word count")
-	count_bytes := flag.Bool("c", false, "Print Byte count")
-	count_chars := flag.Bool("m", false, "Print Character count")
-	flag.Parse()
-	files := flag.Args()
+var (
+	countLines    bool
+	countWords    bool
+	countBytes    bool
+	countChars    bool
+	filesToParse  []string
+	standardInput *bufio.Scanner
+)
 
-	for _, file := range files {
-		if *count_lines {
-			line_count := wc.LinesInAFile(file)
-			fmt.Printf("%d\t", line_count)
-		}
-		if *count_words {
-			word_count := wc.Count(file, bufio.ScanWords)
-			fmt.Printf("%d\t", word_count)
-		}
-		if *count_bytes {
-			byte_count := wc.BytesInAFile(file)
-			fmt.Printf("%d\t", byte_count)
-		}
-		if *count_chars {
-			char_count := wc.Count(file, bufio.ScanRunes)
-			fmt.Printf("%d\t", char_count)
-		}
-		if flag.NFlag() == 0 {
-			line_count := wc.LinesInAFile(file)
-			fmt.Printf("%d\t", line_count)
-			word_count := wc.Count(file, bufio.ScanWords)
-			fmt.Printf("%d\t", word_count)
-			byte_count := wc.BytesInAFile(file)
-			fmt.Printf("%d\t", byte_count)
-		}
-		fmt.Println(file)
+func init() {
+	flag.BoolVar(&countLines, "l", false, "Print Line count")
+	flag.BoolVar(&countWords, "w", false, "Print Word count")
+	flag.BoolVar(&countBytes, "c", false, "Print Byte count")
+	flag.BoolVar(&countChars, "m", false, "Print Character count")
+	flag.Parse()
+	filesToParse = flag.Args()
+	if flag.NFlag() == 0 {
+		flag.Set("l", "true")
+		flag.Set("w", "true")
+		flag.Set("c", "true")
+	}
+	if len(filesToParse) == 0 {
+		standardInput = bufio.NewScanner(os.Stdin)
 	}
 }
+
+func main() {
+	if standardInput == nil {
+		for _, file := range filesToParse {
+			fs := ScanFile(file)
+			countData := wc.CountAll(*fs)
+			PrintCounts(countData)
+			fmt.Println(file)
+		}
+	} else {
+		countData := wc.CountAll(*standardInput)
+		PrintCounts(countData)
+	}
+}
+
+func PrintCounts(res wc.ScanResult) {
+	if countLines {
+		fmt.Printf("%d\t", res.LineCount)
+	}
+	if countWords {
+		fmt.Printf("%d\t", res.WordCount)
+	}
+	if countBytes {
+		fmt.Printf("%d\t", res.ByteCount)
+	}
+	if countChars {
+		fmt.Printf("%d\t", res.CharCount)
+	}
+}
+
+func ScanFile(filepath string) *bufio.Scanner {
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	return scanner
+}
+
+func Analyze(scanner bufio.Scanner) {
+	if countLines {
+		line_count := wc.Count(scanner, bufio.ScanLines)
+		fmt.Printf("%d\t", line_count)
+	}
+	if countWords {
+		word_count := wc.Count(scanner, bufio.ScanWords)
+		fmt.Printf("%d\t", word_count)
+	}
+	if countBytes {
+		// byte_count := wc.BytesInAFile(file)
+		byte_count := wc.Count(scanner, bufio.ScanBytes)
+		fmt.Printf("%d\t", byte_count)
+	}
+	if countChars {
+		char_count := wc.Count(scanner, bufio.ScanRunes)
+		fmt.Printf("%d\t", char_count)
+	}
+	// if flag.NFlag() == 0 {
+	// 	// scanner := ScanFile(file)
+	// 	line_count := wc.LinesInAFile(file)
+	// 	fmt.Printf("%d\t", line_count)
+	// 	word_count := wc.Count(*scanner, bufio.ScanWords)
+	// 	fmt.Printf("%d\t", word_count)
+	// 	byte_count := wc.BytesInAFile(file)
+	// 	fmt.Printf("%d\t", byte_count)
+	// }
+	// if len(files) == 0 {
+	// 	sc := bufio.NewScanner(os.Stdin)
+	// 	// call the respective flag method
+	// }
+	// fmt.Println(file)
+}
+
+// func main() {
+// 	// sc := bufio.NewScanner(os.Stdin)
+// 	// for sc.Scan() {
+// 	// 	fmt.Print(sc.Text())
+// 	// }
+// 	count_lines := flag.Bool("l", false, "Print Line count")
+// 	count_words := flag.Bool("w", false, "Print Word count")
+// 	count_bytes := flag.Bool("c", false, "Print Byte count")
+// 	count_chars := flag.Bool("m", false, "Print Character count")
+// 	flag.Parse()
+// 	files := flag.Args()
+
+// 	for _, file := range files {
+// 		if *count_lines {
+// 			line_count := wc.LinesInAFile(file)
+// 			fmt.Printf("%d\t", line_count)
+// 		}
+// 		if *count_words {
+// 			scanner := ScanFile(file)
+// 			word_count := wc.Count(*scanner, bufio.ScanWords)
+// 			fmt.Printf("%d\t", word_count)
+// 		}
+// 		if *count_bytes {
+// 			byte_count := wc.BytesInAFile(file)
+// 			fmt.Printf("%d\t", byte_count)
+// 		}
+// 		if *count_chars {
+// 			scanner := ScanFile(file)
+// 			char_count := wc.Count(*scanner, bufio.ScanRunes)
+// 			fmt.Printf("%d\t", char_count)
+// 		}
+// 		if flag.NFlag() == 0 {
+// 			scanner := ScanFile(file)
+// 			line_count := wc.LinesInAFile(file)
+// 			fmt.Printf("%d\t", line_count)
+// 			word_count := wc.Count(*scanner, bufio.ScanWords)
+// 			fmt.Printf("%d\t", word_count)
+// 			byte_count := wc.BytesInAFile(file)
+// 			fmt.Printf("%d\t", byte_count)
+// 		}
+// 		if len(files) == 0 {
+// 			sc := bufio.NewScanner(os.Stdin)
+// 			// call the respective flag method
+// 		}
+// 		fmt.Println(file)
+// 	}
+// }
--- wc/wc.go
+++ wc/wc.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 // Returns number of bytes in a file
@@ -40,23 +42,44 @@ func LinesInAFile(filepath string) int {
 
 // Counts number of tokens in a file.
 // Tokenization is done based on bufio.SplitFunc type
-func Count(filepath string, split bufio.SplitFunc) int {
+func Count(scanner bufio.Scanner, split bufio.SplitFunc) int {
 	count := 0
-	f, err := os.Open(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
 	// Set the split function for the scanning operation
 	scanner.Split(split)
 	for scanner.Scan() {
 		count++
 	}
-	err = scanner.Err()
+	err := scanner.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
 	return count
+}
+
+type ScanResult struct {
+	LineCount int
+	WordCount int
+	ByteCount int
+	CharCount int
+}
+
+func CountAll(scanner bufio.Scanner) ScanResult {
+	lineCount, wordCount, byteCount, charCount := 0, 0, 0, 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineCount++
+
+		charCount += utf8.RuneCountInString(line)
+
+		byteCount += len(line)
+
+		words := strings.Split(line, " ")
+		wordCount += len(words)
+	}
+	return ScanResult{
+		LineCount: lineCount,
+		WordCount: wordCount,
+		ByteCount: byteCount,
+		CharCount: charCount,
+	}
 }
